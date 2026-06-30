2ba50f14827f116b04e143cc774f187826295157
Sydney Shaughnessy <shaughnessysydney@gmail.com>
2026-06-30 19:54:53 -0400

    added compatibility tester

---
+++ haskell/Compatibility_Test/Main.hs
@@ -0,0 +1,63 @@
+﻿import System.IO (hFlush, stdout)
+import Data.Char (ord)
+
+main :: IO ()
+main = do
+  putStrLn "<3 Welcome to the Compatibility Tester! <3"
+  putStrLn "" 
+  compatibilityLoop
+
+-- actual test loop
+compatibilityLoop :: IO ()
+compatibilityLoop = do
+  putStr "Enter the first name: "
+  hFlush stdout
+  name1 <- getLine
+  putStr "Enter the second name: "
+  hFlush stdout
+  name2 <- getLine
+  let score = calculateCompatibility name1 name2
+  putStrLn "" 
+  putStrLn $ "The Romance Rating between " ++ name1 ++ " and " ++ name2 ++ " is: " ++ show score ++ "% - " ++ compatibilityComment score
+  putStrLn ""  
+  printStarRating score
+
+  -- do we loop again
+  putStr "Test another pair of names? (yes/no): "
+  hFlush stdout
+  answer <- getLine
+  putStrLn "" 
+  if answer == "yes"
+    then compatibilityLoop
+    else do
+      putStrLn "Thank you for using the Compatibility Tester!!!"
+      putStrLn "" 
+
+--compatibility score calculation
+calculateCompatibility :: String -> String -> Int
+calculateCompatibility name1 name2 = 
+  let totalAscii = sum $ map ord (name1 ++ name2) --converts both names to ints
+      score = (totalAscii `mod` 101)  --results ints to a percentage
+  in score
+
+-- comment based on the compatibility score
+compatibilityComment :: Int -> String
+compatibilityComment score
+  | score < 1 = "Couldn't be worse."
+  | score < 9 = "Hopeless."
+  | score < 19 = "Rarely Reconciled!"
+  | score < 29 = "Just Stay Friends!"
+  | score < 49 = "Worse Than It Looks!"
+  | score < 60 = "Some potential."
+  | score < 69 = "Cutest Couple."
+  | score < 80 = "Lovey-Dovey Duo!"
+  | score < 99 = "Star-Crossed Lovers! <3"
+  | otherwise   = "Perfect match! <3 <3"
+
+-- Print a star rating based on the compatibility score
+printStarRating :: Int -> IO ()
+printStarRating score = do
+  let maxStars = 5  -- Maximum number of stars
+      starScore = (score * maxStars) `div` 100  -- Calculate how many stars to show
+      stars = replicate starScore '★' ++ replicate (maxStars - starScore) '☆'  -- Create the star string
+  putStrLn $ "Star Rating: " ++ stars
---
+++ haskell/Compatibility_Test/README.txt
@@ -0,0 +1,24 @@
+READ ME
+
+This compatibility tester calculates two individuals romantic compatibility based on their names' as a string ASCII int value. It provides a percentage and star rating.
+The concept of the code is adapted from the Tomodachi Life compatibility tester. 
+(While I initially intended to create the dinosaur jumping game, I could not get the frames to update, and I got too frustrated.)
+
+INSTALLATION INSTRUCTIONS
+A Haskell environment is required to run this program
+Download GHC via https://www.haskell.org/ghcup/
+Install GHC through Command Prompt: ghcup install ghc
+Install Cabal after: ghcup install cabal
+Clone the repo: git clone https://github.com/Knox-College-Computer-Science/haskell-project-shaughnessy-sydney
+cd haskell-project-shaughnessy-sydney
+Compile the code: cabal build
+Run the code: cabal run
+
+
+LIMITATIONS
+1) Can't test more than two names at a time.
+2) The results are arbitrary and use no real factors.
+3) Lacks GUI
+
+Disclaimer
+This is not a genuine tool.
